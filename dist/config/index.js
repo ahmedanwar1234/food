5f71cb5cec276896abb5aafdb6b7061b3efdcd7b
ahmedanwar123 <ahmedalprenses@gmail.com>
2024-02-26 21:01:53 +0200

    fucker

---
+++ dist/config/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORT = exports.APP_SECRET = exports.MONGO_URL = void 0;
+exports.MONGO_URL = "mongodb+srv://ahmedanswar123:TUF3K7T0WGQD8utE@cluster0.wabnexf.mongodb.net/?retryWrites=true&w=majority";
+exports.APP_SECRET = "Our_App_Secret";
+exports.PORT = process.env.PORT || 3000;
+//# sourceMappingURL=index.js.map
